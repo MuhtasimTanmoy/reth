922cc0938cedfb95cf0cd2b49d3df79f9605b3f9
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-04-27 15:58:50 +0200

    wip

--- crates/rpc/rpc/src/trace.rs
+++ crates/rpc/rpc/src/trace.rs
@@ -25,7 +25,7 @@ use reth_revm::{database::StateProviderDatabase, db::CacheDB};
 use reth_rpc_api::TraceApiServer;
 use reth_rpc_eth_api::{helpers::TraceExt, FromEthApiError};
 use reth_rpc_eth_types::{error::EthApiError, utils::recover_raw_transaction, EthConfig};
-use reth_storage_api::{BlockNumReader, BlockReader, OmmersProvider};
+use reth_storage_api::{BlockNumReader, BlockReader};
 use reth_tasks::pool::BlockingTaskGuard;
 use reth_transaction_pool::{PoolPooledTx, PoolTransaction, TransactionPool};
 use revm::DatabaseCommit;
